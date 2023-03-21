d9589dbd09a1599d747208f717f814b32e197d4c
Lucia Sarni <lucia@voorhoede.nl>
2023-03-21 14:43:00 +0100

    Add zoom, fit bounds and search controls to map

--- src/components/AppMap/AppMap.vue
+++ src/components/AppMap/AppMap.vue
@@ -6,14 +6,31 @@
       map-style="mapbox://styles/siggyf/ckww2c33f0xlf15nujlx41fe2"
       :center="mapConfig.center"
       :zoom="mapConfig.zoom"
+      @mb-created="onMapCreated"
     >
       <v-mapbox-layer :options="layer" />
+
+      <v-mapbox-geocoder />
+      <map-control-fitbounds
+        :fitToBounds="fitToBounds"
+        position="bottom-right"
+      />
+      <v-mapbox-navigation-control
+        :options="mapConfig.navigationOptions"
+        position="bottom-right"
+      />
     </v-mapbox>
   </div>
 </template>
 
 <script>
+import MapControlFitbounds from "@/components/MapboxMap/MapControlFitbounds.vue";
+
 export default {
+  components: {
+    MapControlFitbounds,
+  },
+
   data() {
     return {
       mapConfig: {
@@ -21,6 +38,9 @@ export default {
         style: "mapbox://styles/siggyf/ckww2c33f0xlf15nujlx41fe2",
         center: [118.69, -3.64],
         zoom: 4,
+        navigationOptions: {
+          showCompass: false,
+        },
       },
       layer: {
         id: "85222873",
@@ -36,6 +56,17 @@ export default {
         paint: {},
       },
     };
+  },
+  methods: {
+    onMapCreated(map) {
+      this.$root.map = map;
+    },
+    fitToBounds() {
+      this.$root.map.flyTo({
+        center: this.mapConfig.center,
+        zoom: this.mapConfig.zoom,
+      });
+    },
   },
 };
 </script>
