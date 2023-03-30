86bb5b7e7704b9883793caa37acb57181deb1c66
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2023-03-30 17:02:44 +0200

    style indicator

---
+++ src/components/ExtendRehabilitatedMangrove/ExtendRehabilitatedMangrove.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-progress-linear
+    :value="progress"
+    height="25"
+    :color="color"
+    class="extend-rehabilitated-mangrove__indicator"
+  >
+    <strong>{{ Math.ceil(progress) }}%</strong>
+  </v-progress-linear>
+</template>
+
+<script>
+export default {
+  props: {
+    progress: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    color() {
+      if (this.progress < 33) {
+        return "red";
+      } else if (this.progress < 66) {
+        return "orange";
+      } else {
+        return "green";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.extend-rehabilitated-mangrove__indicator {
+  border-radius: 0 !important;
+}
+</style>
